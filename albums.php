7a057ee456a81caabbebf8a1f1fabb9d502a02f1
chariesa <mFSY273x4X2E>
2018-07-03 11:12:10 +0200

    Initial commit

---
+++ albums.php
@@ -0,0 +1,180 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>myband</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<nav id="nav">
+    <input type="checkbox" id="menuToggle">
+    <label for="menuToggle" class="menu-toggle">
+        Menu
+        <i class="fas fa-bars"></i>
+        <i class="fas fa-times"></i>
+    </label>
+    <ul>
+        <li><a href="index.php"><img class="logo" src="The_maine_logo.png"></a></li>
+        <li><a href="#">Nieuws</a> </li>
+        <li><a href="Agenda.php">Agenda</a></li>
+        <li><a href="albums.php">Albums</a></li>
+        <li><a href="#">Shop</a></li>
+        <li class="heeft-submenu"><a href="#">Over ons</a>
+            <ul>
+                <li><a href="#">Aanmelden</a>
+                <li><a href="#">Registreren</a>
+                <li><a href="#">Uitloggen</a>
+            </ul>
+        </li>
+        <li><a href="#">Contact</a></li>
+        <li><a href="#">Zoeken</a></li>
+
+    </ul>
+</nav>
+<main>
+
+    <div class="cover">
+        <img src="themain_american_candy.jpg" alt="The maine - American Candy" />
+        <article>
+            <h3> The Maine</h3>
+            <h4> American Candy</h4>
+            <ul>
+                <li> Miles Away                 -  3:38</li>
+                <li> Same Suit, Different Tie   -  3:18</li>
+                <li> My Hair                    -  3:15</li>
+                <li> English Girls              -  3:13</li>
+                <li> 24 Floors                  -  3:43</li>
+                <li> Diet Soda Society          -  3:00</li>
+                <li> Am I Pretty?               -  3:02</li>
+                <li> (Un) Lost                  -  3:45</li>
+                <li> American Candy             -  3:48</li>
+                <li> Another Night on Mars      -  4:26</li>
+            </ul>
+        </article>
+    </div>
+
+    <!-- einde van album-->
+
+
+    <div class="cover">
+        <img src="themain_black_and_white.jpg" alt="The maine - Black and white" />
+        <article>
+            <h3> The Maine</h3>
+            <h4> Black and white</h4>
+            <ul>
+                <li> Don't stop me now                  -  3:36</li>
+                <li> Right girl                         -  3:37</li>
+                <li> Growing up                         -  4:00</li>
+                <li> Fuel to the Fire                   -  3:11</li>
+                <li> Inside of you                      -  3:51</li>
+                <li> Every Road                         -  3:37</li>
+                <li> Lister to your heart               -  3:15</li>
+                <li> Saving Grace                       -  3:53</li>
+                <li> Give it to me                      -  2:42</li>
+                <li> Color                              -  3:41</li>
+                <li> Right Girl (accoustic version)     -  3:24</li>
+                <li> Inside of you (accoustic version)  -  3:10</li>
+            </ul>
+        </article>
+    </div>
+
+    <div class="cover">
+        <img src="hqdefaultkopie.jpg" alt="The Maine Pioneer" />
+        <article>
+            <h3> The Maine</h3>
+            <h4> Pioneer</h4>
+            <ul>
+                <li> Identify                           -  3:37</li>
+                <li> My Heroine                         -  3:36</li>
+                <li> Time                               -  3:29</li>
+                <li> Some Days                          -  3:55</li>
+                <li> I'm Sorry                          -  4:21</li>
+                <li> Don't Give Up on Us                -  3:36</li>
+                <li> Misery                             -  3:53</li>
+                <li> When I'm at Home                   -  4:26</li>
+                <li> Thinking of You                    -  3:03</li>
+                <li> Jenny                              -  4:15</li>
+                <li> Like We Did (Windows Down)         -  4:08</li>
+                <li> While Listening to Rock & Roll     -  4:47</li>
+                <li> Waiting for My Sun to Shine        -  14:43</li>
+            </ul>
+        </article>
+    </div>
+
+    <div class="cover">
+        <img src="the-maine-new1.jpg" alt="The Maine Forever Halloween" />
+        <article>
+            <h3> The Maine</h3>
+            <h4> Forever Halloween</h4>
+            <ul>
+                <li> Take What You Can Carry            -  3:00</li>
+                <li> Love & Drugs                       -  3:30</li>
+                <li> Run                                -  3:39</li>
+                <li> White Walls                        -  4:23</li>
+                <li> Happy                              -  2:43</li>
+                <li> Birthday in Los Angeles            -  3:17</li>
+                <li> Blood Red                          -  3:18</li>
+                <li> Kennedy Curse                      -  3:48</li>
+                <li> Sad Songs                          -  3:21</li>
+                <li> F**ked Up Kids                     -  4:35</li>
+                <li> These Four Words                   -  4:08</li>
+                <li> Forever Halloween                  -  5:26</li>
+            </ul>
+        </article>
+    </div>
+
+    <div class="cover">
+        <img src="268x0wkopie.jpg" alt="The Maine Lovely Little Lonely" />
+        <article>
+            <h3> The Maine</h3>
+            <h4> Lovely Little Lonely</h4>
+            <ul>
+                <li> Don't Come Down                    -  3:13</li>
+                <li> Bad Behavior                       -  3:10</li>
+                <li> Lovely                             -  0:34</li>
+                <li> Black Butterflies and Déjà Vu      -  3:23</li>
+                <li> Taxi                               -  3:07</li>
+                <li> Do You Remember?                   -  3:02</li>
+                <li> Little                             -  1:13</li>
+                <li> The Sound of Reverie               -  3:30</li>
+                <li> Lost in Nostalgia                  -  1:40</li>
+                <li> I Only Wanna Talk to You           -  4:33</li>
+                <li> Lonely                             -  2:32</li>
+                <li> How Do You Feel                    -  4:22</li>
+            </ul>
+        </article>
+    </div>
+
+    <div class="cover">
+        <img src="2285108.jpg" alt="The Maine Can't Stop, Won't Stop" />
+        <article>
+            <h3> The Maine</h3>
+            <h4> Can't Stop, Won't Stop</h4>
+            <ul>
+                <li> Everything I Ask For               -  2:37</li>
+                <li> We All Roll Along                  -  3:49</li>
+                <li> Girls Do What They Want            -  3:10</li>
+                <li> I Must Be Dreaming                 -  2:46</li>
+                <li> Into Your Arms                     -  3:59</li>
+                <li> Time to Go                         -  2:55</li>
+                <li> This Is the End                    -  2:54</li>
+                <li> Whoever She Is                     -  3:41</li>
+                <li> Count'em One, Two, Three           -  2:54</li>
+                <li> Kiss and Sell                      -  3:07</li>
+                <li> You Left Me                        -  3:29</li>
+                <li> We'll All Be                       -  5:29</li>
+            </ul>
+        </article>
+    </div>
+
+    </div>
+    <!-- einde van album-->
+    </div>
+</main>
+<script src="blijfOpPagina.js"></script>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>myband</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<nav id="nav">
+    <input type="checkbox" id="menuToggle">
+    <label for="menuToggle" class="menu-toggle">
+        <i class="fas fa-bars"></i>
+        <i class="fas fa-times"></i>
+    </label>
+    <ul>
+        <li><a href="index.php"><img class="logo" src="The_maine_logo.png"></a></li>
+        <li><a href="#">Nieuws</a> </li>
+        <li><a href="Agenda.php">Agenda</a></li>
+        <li><a href="albums.php">Albums</a></li>
+        <li><a href="#">Shop</a></li>
+        <li class="heeft-submenu"><a href="#">Over ons</a>
+        <ul>
+            <li><a href="#">Aanmelden</a>
+            <li><a href="#">Registreren</a>
+            <li><a href="#">Uitloggen</a>
+        </ul>
+        </li>
+        <li><a href="#">Contact</a></li>
+        <li><a href="#">Zoeken</a></li>
+
+    </ul>
+</nav>
+<main>
+    <h1 id="kop"> Welkom op onze site</h1>
+    <p>Het menu heeft de volgende eigenschappen:</p>
+    <ul>
+        <li>Responsive;</li>
+        <li>Het menu heeft een submenu;</li>
+        <li>Er zijn roll-overs;</li>
+        <li>Voor mobile weergave kan het menu worden ve</li>
+    </ul>
+</main>
+<script src="blijfOpPagina.js"></script>
+
+</body>
+</html>
